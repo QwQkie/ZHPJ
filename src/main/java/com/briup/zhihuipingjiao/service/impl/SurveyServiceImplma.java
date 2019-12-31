29b6833bd1527b7d060f86994f1a69c279985526
marb <1430109296@qq.com>
2019-12-31 17:21:03 +0800

    mrb

--- src/main/java/com/briup/zhihuipingjiao/service/impl/SurveyServiceImplma.java
+++ src/main/java/com/briup/zhihuipingjiao/service/impl/SurveyServiceImplma.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 @Service
-public class SurveyServiceImpl implements ISurveyServicema {
+public class SurveyServiceImplma implements ISurveyServicema {
     @Autowired
     private SurveyEXmaMapper surveyEXMapper;
     @Override
